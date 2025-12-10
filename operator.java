316301c9dd8a0eee0c441e34b6b8742f53a0d0b2
VirajShinkar007 <virajdshinkar@gmail.com>
2025-12-10 12:11:20 +0530

    change

--- operator.java
+++ operator.java
@@ -2,8 +2,11 @@ public class operator {
 
      public static void main(String[] args) {
         int a = 10;
-        int b = 20;
+        int b = 40;
         System.out.println("Addition: " + (a + b));
      }
 }
 //hello
+
+
+
