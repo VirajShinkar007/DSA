fe49f2c4b929bf0959a8e874ae7969071d4ebf1a
VirajShinkar007 <virajdshinkar@gmail.com>
2025-12-10 12:45:07 +0530

    updated operator code

--- operator.java
+++ operator.java
@@ -1,9 +1,12 @@
 public class operator {
 
      public static void main(String[] args) {
-        int a = 10;
-        int b = 40;
-        System.out.println("Addition: " + (a + b));
+      //   int a = 10;
+      //   int b = 40;
+      //   System.out.println("Addition: " + (a + b));
+      int a = 90;
+      int b = 20;
+      System.out.println("remainder " + (a % b));
      }
 }
 //hello
