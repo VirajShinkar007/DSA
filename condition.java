a3272362a01db9fa4cea9801afcd19ee015adc4d
VirajShinkar007 <virajdshinkar@gmail.com>
2025-12-10 13:55:40 +0530

    condition

---
+++ condition.java
@@ -0,0 +1,31 @@
+public class condition {
+    
+    // public static void main(String[] args) {
+    //     int age=18;
+    //     if(age>=18){
+    //         System.out.println("you can vote");
+    //     }
+    //     else if(age>13 && age<18){
+    //         System.out.println("you are teenager");
+    //     }
+    //     else{
+    //         System.out.println("you cant vote");
+    //     }
+
+        
+
+    // }
+
+    //largest of two
+    public static void main(String[] args) {
+        int a=30;
+        int b=40;
+        if(a>=b){
+            System.out.println("a is largest two");
+        }
+        else{
+            System.out.println("b is largest two");
+        }
+   
+    }
+}
--- operator.java
+++ operator.java
@@ -22,13 +22,46 @@ public class operator {
 
      //relational operator
 
+     // public static void main(String[] args) {
+     //     int a = 10;
+     //     int b = 20;
+     //    System.out.println(a==b);
+     //    System.out.println(a>b);
+     //    System.out.println(a!=b);
+     //    System.out.println(a>=b);
+     // }
+
+     //logical operator
+     // public static void main(String[] args) {
+     //   //logical and 
+     //   System.out.println((3>2) && (5>2)); //ture
+     //   System.out.println((3>5) && (5<2)); //false
+     //       //logical or
+     //   System.out.println((3>2) || (5>2)); //true
+     //   System.out.println((3>5) || (5<2)); //false
+
+     //   //logical not
+     //       System.out.println(!(3>2)); //false
+     // }
+
+     //assignment operatot
      public static void main(String[] args) {
-         int a = 10;
-         int b = 20;
-        System.out.println(a==b);
-        System.out.println(a>b);
-        System.out.println(a!=b);
-        System.out.println(a>=b);
+          
+           int a = 10;
+          //  a += 5; // a = a + 5
+          //  System.out.println("After += : " + a);
+           
+          a -= 3; // a = a - 3
+          System.out.println(a);
+           
+           a *= 2; // a = a * 2
+           System.out.println("After *= : " + a);
+           
+           a /= 4; // a = a / 4
+           System.out.println("After /= : " + a);
+           
+           a %= 3; // a = a % 3
+           System.out.println("After %= : " + a); 
      }
 }
 
