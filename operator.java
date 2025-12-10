c494c0c5447f68cceae50f18a3b818f49d1e36e7
VirajShinkar007 <virajdshinkar@gmail.com>
2025-12-10 12:54:41 +0530

    unary operator

--- operator.java
+++ operator.java
@@ -4,12 +4,23 @@ public class operator {
       //   int a = 10;
       //   int b = 40;
       //   System.out.println("Addition: " + (a + b));
-      int a = 90;
-      int b = 20;
-      System.out.println("remainder " + (a % b));
+     //  int a = 90;
+     //  int b = 20;
+     //  System.out.println("remainder " + (a % b));
+
+     // int a=10;
+     // int b=++a;
+     // System.out.println(a);
+     // System.out.println(b);
+
+     int a=10;
+     int b=a--;
+     System.out.println(a);
+     System.out.println(b);
      }
+     
 }
-//hello
+
 
 
 
