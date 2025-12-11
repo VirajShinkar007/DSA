f57fd868b47c02501a60fa6e544b908668ff4bea
VirajShinkar007 <virajdshinkar@gmail.com>
2025-12-11 18:36:48 +0530

    prime no

--- Loops.java
+++ Loops.java
@@ -39,15 +39,74 @@ public class Loops {
     // }
 
     //print revers of the no
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        while(n>0){
-            int lastdigit=n%10;
-            System.out.print("" + lastdigit);
-            n=n/10;
-        }
-        System.out.println("");
+    // public static void main(String[] args) {
+    //     Scanner sc=new Scanner(System.in);
+    //     int n=sc.nextInt();
+    //     while(n>0){
+    //         int lastdigit=n%10;
+    //         System.out.print("" + lastdigit);
+    //         n=n/10;
+    //     }
+    //     System.out.println("");
    
+    // }
+
+    //reverse 
+    // public static void main(String[] args) {
+    //     int n= 666778;
+    //     int rev=0;
+    //     while(n>0){
+    //         int lastdigit=n%10;
+    //         rev=(rev*10)+lastdigit;
+    //         n=n/10;
+    //     }
+        
+    // System.out.println(rev);
+    // }
+
+    //do while loop
+
+    //enter the no while user enter the multiple of 10
+    // public static void main(String[] args) {
+    //     Scanner sc=new Scanner(System.in);
+    //     do{
+    //         System.out.println("enter your no");
+    //         int n=sc.nextInt();
+    //         if(n%10==0){
+    //             break;
+    //         }
+    //         System.out.println(n);
+    //     }
+    //     while(true);
+
+    // }
+
+    //continue keyword
+//     public static void main(String[] args) {
+//         for(int i=1;i<=5;i++){
+//        if(i==3){
+//         continue;
+//        }
+//        System.out.println(i);
+       
+//     }
+// }
+
+public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    boolean isPrime=true;
+    for(int i=2;i<n-1;i++){
+        if(n%2==0){
+            isPrime=false;
+            break;
+        }
+        if(isPrime==false){
+            System.out.println("not prime");
+        }
+        else{
+            System.out.println("prime");    
+        }
     }
+}
 }
