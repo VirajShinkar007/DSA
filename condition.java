d4b0877189953dd61f27a55b150e28c3e38e5fdf
VirajShinkar007 <virajdshinkar@gmail.com>
2025-12-11 11:13:19 +0530

    switch

--- condition.java
+++ condition.java
@@ -44,22 +44,66 @@ public class condition {
     // }
 
     //income tax calc
-    public static void main(String[] args) {
-      Scanner sc=new Scanner(System.in);
-      int income = sc.nextInt();
+    // public static void main(String[] args) {
+    //   Scanner sc=new Scanner(System.in);
+    //   int income = sc.nextInt();
 
-      if(income<=500000){
-        System.out.println("no tax ");
+    //   if(income<=500000){
+    //     System.out.println("no tax ");
 
-      }
-      else if(income>500000 && income <= 1000000){
-        int tax = (int)(income*0.02);
-        System.out.println("tax is " + tax);
+    //   }
+    //   else if(income>500000 && income <= 1000000){
+    //     int tax = (int)(income*0.02);
+    //     System.out.println("tax is " + tax);
 
-      }
-     else{
-        int tax = (int)(income*0.03);
-        System.out.println("tax is " + tax);
-      }
-    }
+    //   }
+    //  else{
+    //     int tax = (int)(income*0.03);
+    //     System.out.println("tax is " + tax);
+    //   }
+    // }
+
+//largenst of three
+    // public static void main(String[] args) {
+    //     int A=10;
+    //     int B=20;           
+    //     int C=15;
+
+    //     if(A>=B && A>=C){
+    //         System.out.println("A is largest");
+    //     }
+    //     else if(B>=C){
+    //         System.out.println("B is largest");
+    //     }
+    //     else{
+    //         System.out.println("C is largest");
+    //     }
+        
+
+    // }
+
+    //ternary operator
+    // public static void main(String[] args) {
+        // int number=45;
+        // String type = (number%2==0) ? "even":"odd";
+        // System.out.println(type);
+        // int marks=75;
+
+        // String result = (marks>35)? "pass":"fail";
+        // System.out.println(result);    }
+
+
+        //switch case
+        public static void main(String[] args) {
+            int caseNum =3;
+            switch(caseNum){
+                case 1: System.out.println("hellow case 1");
+                break;
+                case 2:System.out.println("hellow case 2");
+                break;
+                case 3: System.out.println("hellow case 3");
+                break;
+                default: System.out.println("invalid case");    
+            }
+        }
 }
