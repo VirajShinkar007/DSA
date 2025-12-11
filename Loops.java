0658bf60b0ae5060f15b4a067a2bcd342d0deff0
VirajShinkar007 <virajdshinkar@gmail.com>
2025-12-11 11:36:14 +0530

    while loop

--- Loops.java
+++ Loops.java
@@ -1,3 +1,15 @@
 public class Loops {
+
+
+    //while loop
+    public static void main(String[] args) {
+        int number=0;
+        while(number<100){
+            System.out.println("hello world");
+            number++;
+
+        }
+        
+    }
     
 }
